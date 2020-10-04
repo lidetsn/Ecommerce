c1f79b1390495c0eaa3cc879ea80b5c1684424a9
unknown <luk4ls@yahoo.com>
2020-10-04 16:47:42 -0700

    the first version of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route}  from "react-router-dom"
+import {Container} from "react-bootstrap"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomeScreen from "./screens/HomeScreen"
+import  ProductScreen from "./screens/ProductScreen"
 //import './App.css';
 
 function App() {
   return (
-    <>
-    <h1>welcome to GELINA</h1>
-    </>
+    <Router>
+    <Header/>
+    <Container>   
+       <main className="py-3">
+         <Route path='/' component={HomeScreen} exact/>
+         <Route path='/product/:id' component={ProductScreen }/>
+        </main>
+    </Container>
+    
+    <Footer/>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {LinkContainer} from "react-router-bootstrap"// this does the same aas Link but rap the nave bar
+import {Container, Row, Col,Navbar,Nav} from "react-bootstrap"
+
+
+const Header = () => {
+    return (
+      
+              <header>
+              <Navbar bg="dark"  variant="dark" expand="lg" collapseOnSelect>
+              <Container>
+                  <LinkContainer to="/">
+                    <Navbar.Brand>GELINA Shop</Navbar.Brand></LinkContainer>
+                    {/* <Navbar.Brand href="/">GELINA Shop</Navbar.Brand> */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                    <LinkContainer to="/cart">
+                    <Nav.Link  ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+
+                    </LinkContainer>
+                    {/* <Nav.Link href="/cart" ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link> */}
+                    <LinkContainer to="/login">
+                    <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
+                    </LinkContainer>
+                    {/* <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link> */}
+                  </Nav>
+    
+             </Navbar.Collapse>
+          </Container>
+      </Navbar>
+            
+           </header>
+    
+      
+    )
+}
+
+export default Header
